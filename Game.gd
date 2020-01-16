882dccda124bb9ec2a4fedabca95ba2f7e477074
Unknown <weisong_ong@mymail.sutd.edu.sg>
2020-01-16 17:21:53 +0800

    Fixed Viewport

--- Game.gd
+++ Game.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+onready var MapMaker = $ViewportContainer/Viewport/Main/MapMaker
+onready var player = preload("res://Player/Rogue.tscn")
+onready var slime = preload("res://Enemy/Slime.tscn")
+onready var HUD = preload("res://HUD/HUD.tscn")
+
 func _ready():
 	#yield(get_tree().create_timer(8), "timeout")
 	pass
-	
 	
 	
 func setTargets():
@@ -11,3 +15,40 @@ func setTargets():
 	$ViewportContainer/Viewport/Camera2D.target = get_tree().get_nodes_in_group("Player")[0]
 	$ViewportContainer2/Viewport/Camera2D.target = get_tree().get_nodes_in_group("Player")[0]
 	$ViewportContainer2/Viewport.world_2d = $ViewportContainer/Viewport.world_2d
+
+func genMap():
+	MapMaker.start()
+	
+func loadScreen():
+	$"Loading Screen".start()
+	
+func quitToMainScreen():
+	MapMaker.clearMap()
+	$Player.queue_free()
+	$"Menu Screen".show()
+	
+	
+func _on_New_Game_Button_pressed():
+	$"Menu Screen".hide()
+	loadScreen()
+	# Generate the map
+	genMap()
+	yield(get_tree().create_timer(5), "timeout")
+	# Setup the HUD
+	var b = HUD.instance()
+	$ViewportContainer/Viewport/Main/CanvasLayer.add_child(b)
+	
+	# Instance the player
+	var p = player.instance()
+	$ViewportContainer/Viewport/Main.add_child(p)
+	p.position = MapMaker.startRoom.position
+	
+	# Instance the slime enemy
+	var e = slime.instance()
+	$ViewportContainer/Viewport/Main.add_child(e)
+	e.position = MapMaker.startRoom.position + Vector2(150, 0)
+	
+	# Update and bind the HUD
+	$"ViewportContainer/Viewport/Main/CanvasLayer/HUD".setup()
+	
+	setTargets()
